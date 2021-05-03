4a5d0889dbeda5ea201d828791c8d4ebeb6468c5
shidqimlna <shidqimlna@gmail.com>
2021-05-03 16:56:42 +0700

    styling

--- src/scripts/components/restaurant-item.js
+++ src/scripts/components/restaurant-item.js
@@ -13,8 +13,16 @@ class RestaurantItem extends HTMLElement {
             <img class="item__thumbnail" src="${this._item.pictureId}" alt="${this._item.name}">
             <div class="item__content">
                 <p class="item__title">${this._item.name}</p>
-                <p class="item__city">${this._item.city}</p>
-                <p class="item__rating">${this._item.rating}</p>
+                <div class="item__detail__container">
+                    <div class="item__detail">
+                        <i class="fas fa-map-marker-alt" style="color: crimson"></i>
+                        <p class="item__city">${this._item.city}</p>
+                    </div>
+                    <div class="item__detail">
+                        <i class="fas fa-star" style="color: gold"></i>
+                        <p class="item__rating">${this._item.rating}</p>
+                    </div>
+                </div>
                 <p class="item__description">${this._item.description}</p>
             </div>
         </article>
